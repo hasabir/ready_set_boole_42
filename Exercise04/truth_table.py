6db162f63c18ac8c4398f54d0286b8f3cadec957
hasabir <sabirhajar@gmail.com>
2025-03-28 13:26:07 +0000

    exercice4 truth table

--- Exercise04/truth_table.py
+++ Exercise04/truth_table.py
@@ -11,43 +11,34 @@ class Truth_table:
             "=": lambda x, y: x == y,
             "!": lambda x: not x, 
         }
-        self.stack = []
+        self.num_of_rows = 0
     
     def _get_table(self):
         try:
-            truth_table = {i: [] for i in self.formula if i.isalpha()}
+            self.truth_table = {i: [] for i in self.formula if i.isalpha()}
             step_size = 1
-            num_of_rows = 2 ** len(truth_table)
+            self.num_of_rows = 2 ** len(self.truth_table)
             
-            for column in reversed(truth_table):
+            for column in reversed(self.truth_table):
                 prev = 1
                 i = 0
-                while i < num_of_rows:
-                    prev = 0 if prev == 1 else 1
+                while i < self.num_of_rows:
+                    prev ^= 1
                     for _ in range(step_size):
-                        truth_table[column].append(prev)
+                        self.truth_table[column].append(prev)
                     i += step_size
-                
                 step_size *= 2
-            self.truth_table = truth_table
+            # self.truth_table = truth_table
         except Exception as e:
             raise e
 
     def check_formula(self):
         try:
-            for i in self.formula:
-                if i not in "01&!|^>=":
-                    if not i.isalpha():
-                        raise SyntaxError("Syntax error")
-                    elif i.lower() == i:
-                        raise SyntaxError("Syntax error")
-                
             seen = set()
-            for char in self.formula:
-                if char.isalpha():
-                    if char in seen:
-                        raise SyntaxError(f"Duplicate character '{char}' found in formula")
-                    seen.add(char)
+            for i in self.formula:
+                if i not in "01&!|^>=" and (not i.isalpha() or i in seen or i.lower() == i):
+                    raise SyntaxError(f"Syntax error")
+                seen.add(i)
         except Exception as e:
             raise e
 
@@ -58,12 +49,10 @@ class Truth_table:
                 if i.isdigit():
                     stack.append(bool(int(i)))
                 else:
-                    stock1 = stack.pop()
                     if i == "!":
-                        stack.append(self.operators[i](stock1))
+                        stack.append(self.operators[i](stack.pop()))
                     else:
-                        stock2 = stack.pop()
-                        stack.append(self.operators[i](stock1, stock2))
+                        stack.append(self.operators[i](stack.pop(), stack.pop()))
             if len(stack) != 1:
                 raise ValueError("Invalid formula")
             return stack.pop()
@@ -75,9 +64,8 @@ class Truth_table:
             self.check_formula()
             self._get_table()
             
-            num_of_rows = 2 ** len(self.truth_table)
             self.truth_table['='] = []
-            for i in range(num_of_rows):
+            for i in range(self.num_of_rows):
                 boolean_formula = ""
                 for j in self.formula:
                     if j.isalpha():
