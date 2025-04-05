6e7c57f0ab07ae765ba2da1ea303a510226280b0
hasabir <sabirhajar@gmail.com>
2025-04-05 14:44:20 +0000

    Exercice 5 : Negation Normal Form

--- Exercise05/negation_normal_form.py
+++ Exercise05/negation_normal_form.py
@@ -1,140 +1,81 @@
-# class NegationNormalForm:
-#     def __init__(self):
-#         ...
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
+from node import Node
 
-    def insert(self, key):
-        if self.data == key:
-            return
-        elif self.data < key:
-            if self.right is None:
-                self.right = Node(key)
-            else:
-                self.right.insert(key)
-        else:  # self.data > key
-            if self.left is None:
-                self.left = Node(key)
-            else:
-                self.left.insert(key)
-    
-    def update_data(self, data):
-        self.data = data
-    
-    
-    # def traverse
-        
-    def display(self):
-        lines, *_ = self._display_aux()
-        for line in lines:
-            print(line)
-
-    def _display_aux(self):
-        """Returns list of strings, width, height, and horizontal coordinate of the root."""
-        # No child.
-        if self.right is None and self.left is None:
-            line = '%s' % self.data
-            width = len(line)
-            height = 1
-            middle = width // 2
-            return [line], width, height, middle
-
-        # Only left child.
-        if self.right is None:
-            lines, n, p, x = self.left._display_aux()
-            s = '%s' % self.data
-            u = len(s)
-            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
-            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
-            shifted_lines = [line + u * ' ' for line in lines]
-            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
-
-        # Only right child.
-        if self.left is None:
-            lines, n, p, x = self.right._display_aux()
-            s = '%s' % self.data
-            u = len(s)
-            first_line = s + x * '_' + (n - x) * ' '
-            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
-            shifted_lines = [u * ' ' + line for line in lines]
-            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
-
-        # Two children.
-        left, n, p, x = self.left._display_aux()
-        right, m, q, y = self.right._display_aux()
-        s = '%s' % self.data
-        u = len(s)
-        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
-        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
-        if p < q:
-            left += [n * ' '] * (q - p)
-        elif q < p:
-            right += [m * ' '] * (p - q)
-        zipped_lines = zip(left, right)
-        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
-        return lines, n + m + u, max(p, q) + 2, n + u // 2
-
-
+OPERATORS = {
+    "^": lambda A, B: f"{A}{B}!&{A}!{B}&|",
+    ">": lambda A, B: f"{A}!{B}|",
+    "=": lambda A, B: f"{A}{B}&{A}!{B}!&|",
+}
 
 
 def pars_formula(formula):
-    if formula[-1] == '!':
-        formula = formula[:-1]
-
-    stack = []
-    for i in formula:
-        if i.isalpha():
-            stack.append(Node(i))
-        elif i in "|&":
-            right_childe = stack.pop()
-            left_childe = stack.pop()
-
-            node = Node(i)
-            node.left = left_childe
-            node.right = right_childe
-            
-            stack.append(node)
-        elif i == '!':
-            stock = stack.pop()
-            stock.update_data(f"{stock.data}!")
-            stack.append(stock)
-        elif i in  "=>^":
-            B = stack.pop().data
-            A = stack.pop().data
-            if i == '=':
-                stack.append(pars_formula(f"{A}{B}&{A}!{B}!&|"))
-            elif i == '>':
-                stack.append(pars_formula(f"{A}!{B}|"))
-            elif i == '^':
-                stack.append(pars_formula(f"{A}{B}!&{A}!{B}&|"))
+    try:
+        if formula[-1] == '!':
+            formula = formula[:-1]
+        for i in formula:
+            if i not in "&!|^>=" and (not i.isalpha() or i.lower() == i):
+                raise SyntaxError(f"Syntax error")
+        stack = []
+        for i in formula:
+            if i.isalpha():
+                stack.append(Node(i))
+            elif i in "|&":
+                node = Node(i)
+                node.right, node.left= stack.pop(), stack.pop()
+                stack.append(node)
+            elif i == '!':
+                stock = stack.pop()
+                stock.update_data(f"{stock.data}!")
+                stack.append(stock)
+            elif i in  "=>^":
+                B = stack.pop().data
+                A = stack.pop().data
+                stack.append(pars_formula(OPERATORS[i](A, B)))
+        if len(stack) != 1:
+            raise ValueError("Invalid formula")
+    except Exception as e:
+        raise e
     return stack.pop()        
-            
+
+
+def collect_nodes(formula_tree, is_negative):
+    if formula_tree is None:
+        return
+    collect_nodes(formula_tree.left, is_negative)
+    collect_nodes(formula_tree.right, is_negative)
+    node = formula_tree.data
+    
+    if node in "|&":
+        node = ('&' if node == '|' else '|') if is_negative else node
+    elif '!' not in node:
+        node = f"{node}!" if is_negative else node
+    else:
+        node = node[:-1] if is_negative else node
+        
+    print(node, end='')
+
             
 def negation_normal_form(formula: str) -> str:
-    formula_tree = pars_formula(formula)
-    formula_tree.display()
+    try:
+        formula_tree = pars_formula(formula)
+        formula_tree.display()
+        print('\n********************************************\n')
+        is_negative = False
+        if formula[-1] == '!':
+            is_negative = True
+        collect_nodes(formula_tree, is_negative)
+        print()
+    except Exception as e:
+        raise e
     
-    # convert_to_rpn(formula_tree)
     
-        
-
-
-
-
-
-
+    
+    
 
             
 def main():
     try:
         formula = input("Enter a boolean formula: ")
-        node = negation_normal_form(formula)
-        # node.display()
-
-        # print(f"Result: {pars_formula(formula)}")
+        negation_normal_form(formula)
     except Exception as e:
         print(f"Error: {e}")
 
